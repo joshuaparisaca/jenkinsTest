6569eae9d71dc9ae5c6f5ce5825ffdfb6fc61061
Parisaca <jparisaca@deloitte.com>
2018-11-02 12:41:16 -0400

    Object stuff I'm working on now

---
+++ objectStuff.py
@@ -0,0 +1,34 @@
+import datetime
+import math
+
+human1_name = "Joshua"
+human1_age = 24
+human1_height = 66
+
+human2_name = "Alex"
+human2_age = 25
+human2_height = 68
+
+def print_human_info(name, age, height):
+    print(name, age, height)
+
+class Human(object): #This creates an object with values passed into this class
+    def __init__(self, name, age, height):
+        self.age = age #You need these attibutes to create an object
+        self.name = name
+        self.height = height
+
+    def print_stats(self): #This prints out the attributes of the values passed to the class/object 
+        print("The human's name is " + self.name + " and their height is " +
+                str(self.height) + " and they are " + str(self.age) + " years old.")
+
+human1 = Human(human1_name, human1_age, human1_height) #Creates an object based on variable values
+human2 = Human(human2_name, human2_age, human2_height)
+
+human1.print_stats() #Prints out the stats based on the variable values the object has
+human2.print_stats()
+
+human_list = []
+for row in rows:
+    human_list.append(Human(row.name, row.age, row.height))
+    #Use this type of syntax to read from a file and input the values of a object into the "human" object and into a list 
--- test.py
+++ test.py
@@ -7,21 +7,37 @@ print("git push -u origin master\n")
 #Test Comment
 
 print("Python Concepts:")
-userOptions = ['Imports','Math','Variables and Names','Strings and Text']
+userOptions = ['Imports','Math','Variables and Names',
+'Strings and Text','Printing','Parameters and Input',
+'Reading Files', 'Writing Files', 'Functions', 'Boolean', 'Try Catch']
+
+userFunctions = [imports(), math(), variables(), ...]
 
 for index, item in enumerate(userOptions):
 	print (index + 1, item)
 
-userInput = int(input("\nPress select one of the options to learn more about these concepts:\n"))
+try:
+	userInput = int(input("\nPress select one of the options to learn more about these concepts:\n"))
+	if userInput <= (index + 1):
+		print("Correct: Under the Limit")
+		print("You've chosen:", userOptions[userInput-1])
+	else:
+		userFunctions[userInput]
+		print("Wrong: Over the Limit")
+except Exception as e:
+	print("This isnt a proper number ", e)
 
-if userInput < (index+1):
-	print("Correct: Under the Limit")
-	print("You've chosen:", userOptions[userInput-1])
-else:
-	print("Wrong: Over the Limit")
 
 #Create class/function names with the names of the concepts and when user input the number, itll activate the class/function name
 
 #while True:
 #    print("This prints once a minute.")
 #    time.sleep(1) # Delay for 1 seconds
+
+#_Function used insides file/module
+#function used in another file/module
+
+#def functionname
+#def __init__ functioname ----------> for function to make objects
+
+#Also check out the Python Library
