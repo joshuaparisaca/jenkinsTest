788d0232b37db036b9ba8c0cd5e05a32f6015d85
Parisaca <jparisaca@deloitte.com>
2018-11-02 16:21:36 -0400

    Added some new methods

--- test.py
+++ test.py
@@ -1,10 +1,35 @@
 import time
+import os
+
+#Make sure to know how to define a global function
+
+def commenting_Explanation():
+	print("# Is used for a single line comment")
+	print("'''")
+	print("These 3 quotes will comment for multiple lines")
+	print("''' \n")
+	print("Both the SingleLine comment and MultiLine comment WILL NOT WORK if it's inside a string")
+
+def math_Explanation():
+	print("Common Math Operators:")
+	print("+  Addition")
+	print("-  Subtraction")
+	print("*  Mulitplication")
+	print("/  Division")
+	print("%  Module")
+	print("<  Less Than")
+	print("<= Less Than or Equal to")
+	print(">  Great Than")
+	print(">= Greater Than or Equal to")
+	print("")	
+	print("Imported Math Stuff:")	
+	print("")	
+	print("")	
+	print("")	
+	print("")	
 
 def imports_Explanation():
 	print("This an Import function")
-
-def math_Explanation():
-	print("This is a Math function")	
 
 def variables_and_names_Explanation():
 	print("This is a Variables and Names function")
@@ -19,9 +44,9 @@ print("git push -u origin master\n")
 
 print("Python Concepts:")
 userFunctions = []
-userOptions = ['Imports','Math','Variables and Names',
+userOptions = ['Commenting','Imports','Math','Variables and Names',
 'Strings and Text','Printing','Parameters and Input',
-'Reading Files', 'Writing Files', 'Functions', 'Boolean', 'Try Catch']
+'Reading Files', 'Writing Files', 'Functions', 'Boolean', 'Try Catch', 'Tips and Tricks', 'Debugging']
 
 for index, item in enumerate(userOptions):
 	renamedItem = item.lower().replace(" ","_") + "_Explanation"
@@ -31,10 +56,10 @@ for index, item in enumerate(userOptions):
 try:
 	userInput = int(input("\nPress select one of the options to learn more about these concepts:\n"))
 	if userInput <= (index + 1):
-		print("Correct: Under the Limit")
-		print("You've chosen:", userOptions[userInput - 1])
-		print(userFunctions[userInput - 1])
-		globals()[userFunctions[userInput - 1]]() #This will call one of the functions based on the string from the userFunctions list
+		print("Here's the explanation for:", userOptions[userInput - 1] + "\n")
+		os.system('cls')
+		globals()[userFunctions[userInput - 1]]() 
+		#This will call one of the functions based on the string from the userFunctions list
 		#globals() means it'll go through other python files to find your function
 		#locals() will try to find your function in this file only
 	else:
